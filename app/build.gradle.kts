2d0d0d05a28c92e099bc5d7689599660d583cbf0
proskurekov <proskurekovdim@gmail.com>
2025-05-29 16:43:02 +0700

    add sonar build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "hexlet.code"
@@ -22,6 +23,14 @@ tasks.test {
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
+
+sonar {
+    properties {
+      property("sonar.projectKey", "proskdim_java-project-61")
+      property("sonar.organization", "proskdim")
+      property("sonar.host.url", "https://sonarcloud.io")
+    }
+  }
 
 application  {
   mainClass = "hexlet.code.App"
